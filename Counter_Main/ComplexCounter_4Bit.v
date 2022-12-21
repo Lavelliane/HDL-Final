fca07c33e45a0fca3b97b955637b5a85cc6cdb6f
Lavelliane <jhurylastre432@gmail.com>
2022-12-21 21:31:21 +0800

    added counter top module

---
+++ Counter_Main/ComplexCounter_4Bit.v
@@ -0,0 +1,75 @@
+/**************************************************
+*	File	: 				ComplexCounter_3Bit.v
+*	Author:				Thomas Lee Arquiza Castro
+*	Class: 				CpE 3101L
+*	Group/Schedule: 	Group 6 Wed 9:00AM-12NN
+*	Description:		HDL Design Entry File
+**************************************************/
+
+module ComplexCounter_4Bit(
+	input wire			Clk, nReset, Enable,
+	input wire [3:0]  Mode,
+	output reg [3:0]	Count
+);
+
+	reg [3:0]	cState, nState;
+	
+	always @ (negedge Clk)
+	begin
+		if (!nReset)
+			cState <= 4'b1001; //9
+		else
+			cState <= nState;
+	end
+	
+	always @ (*)
+	begin
+		if (Enable)
+			begin
+				if (Mode == 4'd9)
+					begin
+						case (cState)
+							4'b1001 : nState <= 4'b1000;  //9
+							4'b1000 : nState <= 4'b0111;  //8
+							4'b0111 : nState <= 4'b0110;  //7
+							4'b0110 : nState <= 4'b0101;  //6
+							4'b0101 : nState <= 4'b0100;  //5
+							4'b0100 : nState <= 4'b0011;  //4
+							4'b0011 : nState <= 4'b0010;  //3
+							4'b0010 : nState <= 4'b0001;  //2
+							4'b0001 : nState <= 4'b0000;  //1
+							default : nState <= 4'b1001;  //0 -> 9
+						endcase
+					end
+				else if (Mode == 4'd5)
+					begin
+						case (cState)
+							4'b0101 : nState <= 4'b0100;  //5
+							4'b0100 : nState <= 4'b0011;  //4
+							4'b0011 : nState <= 4'b0010;  //3
+							4'b0010 : nState <= 4'b0001;  //2
+							4'b0001 : nState <= 4'b0000;  //1
+							default : nState <= 4'b0101;  //0 -> 5
+						endcase
+					end
+				else if (Mode == 4'd1)
+					begin
+						case (cState)
+							4'b0001 : nState <= 4'b0000;  //1
+							default : nState <= 4'b0101;  //0 -> 1
+						endcase 
+					end
+				else
+					nState <= 4'b0000;
+			end
+		else 
+			nState <= 4'b0000;
+	end
+	
+	always @ (*)
+		begin
+				Count <= cState;
+		end
+	
+endmodule 
+
--- Counter_Main/Counter_Main.v
+++ Counter_Main/Counter_Main.v
@@ -2,4 +2,18 @@
 //CounterEnable: 1-bit input to determine if counter is on (Only on if coin is inserted)
 
 
-module Counter_Main(CounterInput, CounterEnable, Clk, nReset, S0, S1, S2);
+module Counter_Main(CounterInput, CounterEnable, Clk, nReset, S1, S2, S3, S4, S5, S6);
+
+	input [3:0]CounterInput;
+	input CounterEnable, Clk, nReset;
+	output [3:0]S1, S2, S3, S4, S5, S6;
+	
+
+
+	ComplexCounter_4Bit C1(.Clk(Clk), .nReset(nReset), .Enable(CounterEnable), .Mode(4'd9), .Count(S1)); //9
+	ComplexCounter_4Bit C2(.Clk(Clk), .nReset(nReset), .Enable(S0 == 4'd0), .Mode(4'd5), .Count(S2));  //5
+	ComplexCounter_4Bit C3(.Clk(Clk), .nReset(nReset), .Enable(S0 == 4'd0), .Mode(CounterInput == 4'd1 ? 4'd1 : 4'd9), .Count(S3)); //1:59
+
+
+			
+endmodule
